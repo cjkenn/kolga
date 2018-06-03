02b2bf0ee3829fb30755d6bcf211ae8d58c96818
cjkenn <cam.j.kennedy@gmail.com>
2018-06-03 14:09:07 -0700

    symtab fns

--- libsnowc/sym.rs
+++ libsnowc/sym.rs
@@ -17,19 +17,21 @@ pub enum SymVal {
 }
 
 pub struct Sym {
-    sym_ty: SymTy,
-    val_ty: Option<ValTy>,
-    name: String,
-    val: Option<SymVal>
+    pub sym_ty: SymTy,
+    pub val_ty: Option<ValTy>,
+    pub name: String,
+    pub val: Option<SymVal>,
+    pub imm: bool
 }
 
 impl Sym {
-    pub fn new(ty: SymTy, vty: Option<ValTy>, n: &str, v: Option<SymVal>) -> Sym {
+    pub fn new(ty: SymTy, vty: Option<ValTy>, n: &str, v: Option<SymVal>, im: bool) -> Sym {
         Sym {
             sym_ty: ty,
             val_ty: vty,
             name: n.to_string(),
-            val: v
+            val: v,
+            imm: im
         }
     }
 }
--- libsnowc/symtab.rs
+++ libsnowc/symtab.rs
@@ -13,4 +13,15 @@ impl SymTab {
             symtab: HashMap::new()
         }
     }
+
+    pub fn store(&mut self, key: &str, sym: Sym) {
+        self.symtab.insert(String::from(key), Rc::new(sym));
+    }
+
+    pub fn retrieve(&self, key: &str) -> Option<Rc<Sym>> {
+        match self.symtab.get(key) {
+            Some(val) => Some(Rc::clone(&val)),
+            None => None
+        }
+    }
 }
