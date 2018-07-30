29fa19dade264a4a01a03b5b6382912db0bb7f9e
cjkenn <cam.j.kennedy@gmail.com>
2018-07-29 20:55:43 -0700

    additional primary types, wrap top level expressions in anon functions.

--- libllvm_codegen/gen.rs
+++ libllvm_codegen/gen.rs
@@ -9,6 +9,7 @@ use kolgac::token::TknTy;
 use kolgac::type_record::TyRecord;
 
 use std::collections::HashMap;
+use std::ptr;
 
 const LLVM_FALSE: LLVMBool = 0;
 const LLVM_TRUE: LLVMBool = 1;
@@ -57,21 +58,32 @@ impl<'t, 's> Gen<'t, 's> {
         match *self.ast {
             Ast::Prog(ref stmts) => {
                 for stmt in stmts {
-                    let val = self.gen_stmt(stmt);
-                    unsafe {
-                        LLVMDumpValue(val);
-                    }
+                    self.gen_stmt(stmt);
                 }
             },
             _ => ()
         }
+
+        unsafe {
+            LLVMDumpModule(self.module);
+        }
     }
 
-    fn gen_stmt(&mut self, stmt: &Ast) -> LLVMValueRef {
+    fn gen_stmt(&mut self, stmt: &Ast) {
         match stmt {
             Ast::ExprStmt(maybe_ast) => {
-                let ast = maybe_ast.clone().unwrap();
-                return self.gen_expr(&ast);
+                unsafe {
+                    // Wrap raw expressions in anonymous functions
+                    let anon_fn_ty = LLVMFunctionType(self.float_ty(), ptr::null_mut(), 0, LLVM_FALSE);
+                    let anon_fn = LLVMAddFunction(self.module, c_str!("_anon"), anon_fn_ty);
+                    let anon_bb = LLVMAppendBasicBlockInContext(self.context, anon_fn, c_str!("_anon"));
+
+                    LLVMPositionBuilderAtEnd(self.builder, anon_bb);
+
+                    let ast = maybe_ast.clone().unwrap();
+                    let val = self.gen_expr(&ast);
+                    LLVMBuildRet(self.builder, val);
+                }
             },
             _ => unimplemented!("Ast type {:?} is not implemented for codegen", stmt)
         }
@@ -82,33 +94,34 @@ impl<'t, 's> Gen<'t, 's> {
             Ast::Primary(prim_ty_rec) => {
                 return self.gen_primary(&prim_ty_rec);
             },
-            Ast::Binary(op_tkn, maybe_lhs, maybe_rhs) => {
+            Ast::Binary(op_tkn, maybe_lhs, maybe_rhs) |
+            Ast::Logical(op_tkn, maybe_lhs, maybe_rhs) => {
                 let lhs_llvm_val = self.gen_expr(&maybe_lhs.clone().unwrap());
                 let rhs_llvm_val = self.gen_expr(&maybe_rhs.clone().unwrap());
 
                 // TODO: check for None values here, when actual error handling is added
-                match op_tkn.ty {
-                    TknTy::Plus => {
-                        unsafe {
+                unsafe {
+                    match op_tkn.ty {
+                        TknTy::Plus => {
                             return LLVMBuildFAdd(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("addtmp"));
-                        }
-                    },
-                    TknTy::Minus => {
-                        unsafe {
+                        },
+                        TknTy::Minus => {
                             return LLVMBuildFSub(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("subtmp"));
-                        }
-                    },
-                    TknTy::Star => {
-                        unsafe {
+                        },
+                        TknTy::Star => {
                             return LLVMBuildFMul(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("multmp"));
-                        }
-                    },
-                    TknTy::Slash => {
-                        unsafe {
+                        },
+                        TknTy::Slash => {
                             return LLVMBuildFDiv(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("divtmp"));
+                        },
+                        TknTy::AmpAmp | TknTy::And => {
+                            return LLVMBuildAnd(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("andtmp"));
+                        },
+                        TknTy::PipePipe | TknTy::Or => {
+                            return LLVMBuildOr(self.builder, lhs_llvm_val, rhs_llvm_val, c_str!("ortmp"));
                         }
-                    },
-                    _ => panic!("Invalid binary operator found in codegen")
+                        _ => panic!("Invalid binary operator found in codegen")
+                    }
                 }
             },
             _ => unimplemented!("Ast type {:?} is not implemented for codegen", expr)
@@ -118,9 +131,13 @@ impl<'t, 's> Gen<'t, 's> {
     fn gen_primary(&mut self, ty_rec: &TyRecord) -> LLVMValueRef {
         match ty_rec.tkn.ty {
             TknTy::Val(ref val) => {
-                unsafe {
-                    return LLVMConstReal(LLVMFloatType(), *val);
-                }
+                unsafe { return LLVMConstReal(LLVMFloatType(), *val); }
+            },
+            TknTy::True => {
+                unsafe { return LLVMConstInt(self.i8_ty(), 1, LLVM_FALSE); }
+            },
+            TknTy::False => {
+                unsafe { return LLVMConstInt(self.i8_ty(), 0, LLVM_FALSE); }
             },
             TknTy::Ident(ref name) => {
                 let val = self.valuetab.get(name);
@@ -133,5 +150,17 @@ impl<'t, 's> Gen<'t, 's> {
             },
             _ => unimplemented!("Tkn ty {:?} in unimplemented in codegen", ty_rec.tkn.ty)
         }
+    }
+
+    fn void_ty(&self) -> LLVMTypeRef {
+        unsafe { LLVMVoidTypeInContext(self.context) }
+    }
+
+    fn float_ty(&self) -> LLVMTypeRef {
+        unsafe { LLVMFloatTypeInContext(self.context) }
+    }
+
+    fn i8_ty(&self) -> LLVMTypeRef {
+        unsafe { LLVMInt8TypeInContext(self.context) }
     }
 }
