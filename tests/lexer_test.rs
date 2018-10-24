cca16291b6cbfb5ea9ef5ce6449e7d938b802bfb
cjkenn <cam.j.kennedy@gmail.com>
2018-10-24 16:56:15 -0700

    lexer tests

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -6,145 +6,174 @@ use kolgac::lexer::Lexer;
 use kolgac::token::TknTy;
 
 #[test]
-fn lex_empty() {
-    let file = File::open("./tests/lexer_input/empty").unwrap();
+fn lexer() {
+    let file = File::open("./tests/lexer/lex").unwrap();
     let mut lexer = Lexer::new(file);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::LeftParen);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::RightParen);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::LeftBrace);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::RightBrace);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::LeftBracket);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::RightBracket);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Semicolon);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Period);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Comma);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Plus);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Minus);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Star);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Percent);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Tilde);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Slash);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Eq);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::EqEq);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Lt);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::LtEq);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Gt);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::GtEq);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Bang);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::BangEq);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Amp);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::AmpAmp);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Pipe);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::PipePipe);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Val(100.0));
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Str(String::from("string")));
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Let);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Imm);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Fn);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Return);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Class);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::This);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::If);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Elif);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Then);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Else);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Else);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::While);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::In);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::For);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Num);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::String);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::String);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Bool);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::True);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::False);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Or);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::And);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Null);
+
+    let tkn = lexer.lex();
+    assert_eq!(tkn.ty, TknTy::Void);
+
     let tkn = lexer.lex();
     assert_eq!(tkn.ty, TknTy::Eof);
-}
 
-#[test]
-fn lex_unrecognized() {
-    let file = File::open("./tests/lexer_input/unrecognized").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Eof);
-}
-
-#[test]
-fn lex_string_lit() {
-    let file = File::open("./tests/lexer_input/stringlit").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Str(String::from("teststr")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-}
-
-#[test]
-fn lex_integer_lit() {
-    let file = File::open("./tests/lexer_input/integerlit").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Val(10.0));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-}
-
-#[test]
-fn lex_float_lit() {
-    let file = File::open("./tests/lexer_input/floatlit").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Val(10.55));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-}
-
-#[test]
-fn lex_unterminated_str_lit() {
-    let file = File::open("./tests/lexer_input/untermstringlit").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Eof);
-}
-
-#[test]
-fn lex_ident() {
-    let file = File::open("./tests/lexer_input/ident").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-}
-
-#[test]
-fn lex_ident_string_lit() {
-    let file = File::open("./tests/lexer_input/identstringlit").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-
-    let tkn2 = lexer.lex();
-    assert_eq!(tkn2.ty, TknTy::Str(String::from("teststr")));
-    assert_eq!(tkn2.line, 1);
-    assert_eq!(tkn2.pos, 6);
-}
-
-#[test]
-fn lex_comment() {
-    let file = File::open("./tests/lexer_input/comment").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Eof);
-}
-
-#[test]
-fn lex_comment_w_line() {
-    let file = File::open("./tests/lexer_input/commentandident").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.lex();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 2);
-    assert_eq!(tkn.pos, 0);
-}
-
-#[test]
-fn peek_tkn_same_line() {
-    let file = File::open("./tests/lexer_input/ident").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.peek_tkn();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-
-    let tkn2 = lexer.lex();
-    assert_eq!(tkn, tkn2);
-}
-
-#[test]
-fn peek_tkn_multiple_peeks() {
-    let file = File::open("./tests/lexer_input/ident").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.peek_tkn();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-
-    let tkn2 = lexer.peek_tkn();
-    assert_eq!(tkn, tkn2);
-}
-
-#[test]
-fn peek_tkn_two_lines() {
-    let file = File::open("./tests/lexer_input/peek_tkn_two_lines").unwrap();
-    let mut lexer = Lexer::new(file);
-    let tkn = lexer.peek_tkn();
-    assert_eq!(tkn.ty, TknTy::Ident(String::from("ident")));
-    assert_eq!(tkn.line, 1);
-    assert_eq!(tkn.pos, 0);
-
-    let tkn2 = lexer.lex();
-    assert_eq!(tkn, tkn2);
-
-    let tkn3 = lexer.peek_tkn();
-    assert_eq!(tkn3.ty, TknTy::Str(String::from("string")));
-    assert_eq!(tkn3.line, 2);
-    assert_eq!(tkn3.pos, 0);
-
-    let tkn4 = lexer.lex();
-    assert_eq!(tkn3, tkn4);
+    println!("PASS: lexing all symbols and keywords");
 }
